edcaed999dc03da598bc77ef3415db46539e843e
CaahFerreira <CAAHFERREIRA.S@GMAIL.COM>
2025-05-19 22:21:53 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("mensagemBtn").addEventListener("click", function() {
+  document.getElementById("mensagemTexto").textContent =
+    "O caminho para o progresso é árduo e solitário, mas a força que você encontra dentro de si é inabalável. Continue subindo de nível, continue evoluindo, mesmo que os outros não consigam acompanhar.";
+});
+
