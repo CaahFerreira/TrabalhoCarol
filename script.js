43172973dfb4a93a9c0d7ab7d0811a0cadd88126
CaahFerreira <CAAHFERREIRA.S@GMAIL.COM>
2025-05-19 22:15:54 -0300

    Delete script.js

--- script.js
+++
@@ -1,3 +0,0 @@
-document.getElementById("mensagemBtn").addEventListener("clique para ver a mensagem", function() {
-  document.getElementById("mensagemTexto").textContent = "O caminho para o progresso é árduo e solitário, mas a força que você encontra dentro de si é inabalável. Continue subindo de nível, continue evoluindo, mesmo que os outros não consigam acompanhar.";
-});
